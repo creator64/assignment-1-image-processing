49604b5dd37b26d05e8d8a3bf8dcc37a44ccf3e9
Dangual <dangual1304@outlook.com>
2025-09-16 18:28:27 +0200

    Added helper functions'

---
+++ Helper Code/HelperFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFOIBV.Helper_Code;
+
+namespace INFOIBV.Helper_Code
+{
+    public enum PaddingMethods : byte
+    {
+        ConstantValue,  // pad the image with a constant value
+        CopyPerimeter,  // pad the image with the same value the neighbour on the perimeter has
+        MirrorImage,    // pad the image with mirrored copies of the image
+        CopyImage       // pad the image with non-mirrored copies of the image
+    }
+
+    /// <summary>
+    /// A static class with helper functions to cut down
+    /// </summary>
+    public static class HelperFunctions
+    {
+        /// <summary>
+        /// Applying an uneven filter 
+        /// </summary>
+        /// <param name="I">The Original Image</param>
+        /// <param name="H">The filter-matrix you want to apply to I</param>
+        /// <param name="method">The padding method you want to apply</param>
+        /// <param name="paddingValue">
+        /// Only relevant when padding with PaddingMethods.ConstantValue.
+        /// Causes the paddingmethod to pad with the given value, defaults to 0 (black)
+        /// </param>
+        /// <returns>The result of I * H</returns>
+        public static byte[,] applyUnevenFilter(byte[,] I, byte[,] H)
+        {
+
+            return I;
+        }
+    }
+}
---
+++ Helper Code/Padders.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFOIBV.Helper_Code
+{
+    public abstract class Padder
+    {
+        protected byte paddingWidth;
+
+        protected Padder(byte paddingWidth)
+        {
+            this.paddingWidth = paddingWidth;
+        }
+
+        public abstract byte[,] padImage(byte[,] image);
+
+    }
+
+    public class ConstantValuePadder : Padder
+    {
+        private byte paddingValue;
+        public ConstantValuePadder(byte paddingWidth, byte paddingValue) : base(paddingWidth)
+        {
+            this.paddingValue = paddingValue;
+        }
+
+        public override byte[,] padImage(byte[,] image)
+        {
+            byte[,] newImage = new byte[image.GetLength(0) + (2 * paddingWidth), image.GetLength(1) + (2 * paddingWidth)];
+
+            for (int i = 0; i < newImage.GetLength(0); i++)
+            {
+                for (int j = 0; j < newImage.GetLength(1); j++)
+                {
+                    if (i >= paddingWidth && j >= paddingWidth && (i - paddingWidth) < image.GetLength(0) && (j - paddingWidth) < image.GetLength(1))
+                        newImage[i, j] = image[i - paddingWidth, j - paddingWidth];
+                    else
+                        newImage[i, j] = paddingValue;
+                }
+            }
+            return newImage;
+        }
+    }
+
+    public class CopyPerimeterPadder : Padder
+    {
+        public CopyPerimeterPadder(byte paddingWidth) : base(paddingWidth) { }
+
+        public override byte[,] padImage(byte[,] image)
+        {
+            byte[,] newImage = new byte[image.GetLength(0) + (2 * paddingWidth), image.GetLength(1) + (2 * paddingWidth)];
+
+            for (int i = 0 i < newImage.GetLength(0); i++)
+            {
+                for (int j = 0; j < newImage.GetLength(1); j++)
+                {
+                    int x = i;
+                    int y = j;
+
+                    if (i < paddingWidth) x = 0;
+                    else if (i >= image.GetLength(0)) x = image.GetLength(0) - 1;
+
+                    if (j < paddingWidth) y = 0;
+                    else if (j >= image.GetLength(1)) y = image.GetLength(1) - 1;
+
+                    newImage[i, j] = image[x, y];
+                }
+            }
+            return newImage;
+        }
+    }
+}
