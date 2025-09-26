77e5a9b77037194d1b062d7638719a33e8bc8a4e
Dangual <dangual1304@outlook.com>
2025-09-26 22:04:40 +0200

    ImageAnalysers rework

--- Helper Code/ImageAnalysers.cs
+++ Helper Code/ImageAnalysers.cs
@@ -7,34 +7,39 @@ using System.Xml.Schema;
 
 namespace INFOIBV.Helper_Code
 {
-    public static class ImageAnalysers
+    public class ImageData
     {
+        public int[] histogram { get; private set; }
+        public float averageIntensity { get; private set; }
+        public ImageData(byte[,] I)
+        {
+            histogram = createHistogram(I);
+            averageIntensity = getAverageIntensityValue(I, histogram);
+        }
+
         /// <param name="I">The image in the form of a 2D byte array</param>
         /// <returns>An array with indices 0 to 255, the value contained in each index is the amount of pixels of that intensity value</returns>
-        public static int[] createHistogram(byte[,] I)
+        private int[] createHistogram(byte[,] I)
         {
             int[] histogram = new int[256];
 
-            for(int i = 0; i < I.GetLength(0); i++)
-                for(int j = 0; j < I.GetLength(1); j++)
+            for (int i = 0; i < I.GetLength(0); i++)
+                for (int j = 0; j < I.GetLength(1); j++)
                     histogram[I[i, j]]++;
 
             return histogram;
         }
 
-
         /// <param name="I">The image for which you want to calculate the average intensity value</param>
-        /// <returns>The average intensity rounded to its nearest integer value</returns>
-        public static int getAverageIntensityValue(byte[,] I)
+        /// <returns>The average intensity</returns>
+        private float getAverageIntensityValue(byte[,] I, int[] histogram)
         {
-            int[] histogram = createHistogram(I);
-
             int totalVal = 0;
 
             for (int i = 0; i < histogram.Length; i++)
                 totalVal += (i * histogram[i]);
 
-            return (int)Math.Round((decimal)totalVal / I.LongLength);
+            return (float)Math.Round((decimal)totalVal / I.LongLength);
         }
     }
 }
